08250d088e46c9cbe36b4367bd0ecc4ee2d6c924
kravtsovnikita <96532057+kravtsovnikita@users.noreply.github.com>
2024-06-23 14:26:28 +0300

    Add files via upload

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,28 +1,28 @@
 ﻿using System;
 
-namespace CelsiusToFahrenheitConversion
+namespace ComparisonProgram
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double c;
-            double f;
-            Console.Write("Введите 1, если необходим перевод в Фаренгейты\r\nВведите 2, если необходим перевод в Цельсия:\r\n");
-            int gr = Convert.ToInt32(Console.ReadLine());
-            if (gr==1)
+            Console.WriteLine("Введите первое число:");
+            int number1 = Convert.ToInt32(Console.ReadLine()); 
+
+            Console.WriteLine("Введите второе число:");
+            int number2 = Convert.ToInt32(Console.ReadLine()); 
+
+            if (number1 == number2)
             {
-                Console.WriteLine("Введите температуру в градусах Цельсия: ");
-                c = Convert.ToDouble(Console.ReadLine());
-                f = c * 1.8 + 32;
-                Console.WriteLine("Температура в Фаренгейтах: " + f);
+                Console.WriteLine("Числа равны");
+            }
+            else if (number1 > number2)
+            {
+                Console.WriteLine($"Первое число больше второго числа");
             }
             else
             {
-                Console.WriteLine("Введите температуру в градусах Фаренгейта: ");
-                f = Convert.ToDouble(Console.ReadLine());
-                c = (f - 32) / 1.8;
-                Console.WriteLine("Температура в Цельсия: " + c);
+                Console.WriteLine($"Первое число меньше второго числа");
             }
         }
     }
